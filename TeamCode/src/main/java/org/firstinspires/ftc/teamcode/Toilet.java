966976dafe5e9722e546fb7e7857887660752f02
abond26 <abond26@klschools.org>
2025-11-20 23:22:11 -0500

    :/

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Toilet.java
@@ -0,0 +1,155 @@
+package org.firstinspires.ftc.teamcode;
+
+//import com.acmerobotics.dashboard.FtcDashboard;
+//import com.acmerobotics.dashboard.config.Config;
+//import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.pedropathing.follower.Follower;
+
+import com.pedropathing.geometry.Pose;
+import com.qualcomm.hardware.limelightvision.Limelight3A;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
+
+
+//@Config
+@TeleOp
+public class Toilet extends LinearOpMode {
+    private Limelight3A limelight;
+    double time;
+    double newTime;
+    boolean xLast = false;
+    private Follower follower;
+    private final Pose startPose = new Pose(0, 0, 0);
+    private DcMotor intake, launcher, flicker, rotator;
+    private DcMotorEx leftFront, leftRear, rightFront, rightRear;
+    public ElapsedTime runtime = new ElapsedTime();
+
+    public void runOpMode() throws InterruptedException{
+
+        leftFront = hardwareMap.get(DcMotorEx.class, "leftFront");
+        leftFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        leftRear = hardwareMap.get(DcMotorEx.class, "leftRear");
+        leftRear.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightFront = hardwareMap.get(DcMotorEx.class, "rightFront");
+        rightFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightRear = hardwareMap.get(DcMotorEx.class, "rightRear");
+        rightRear.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        follower = Constants.createFollower(hardwareMap);
+        follower.setStartingPose(startPose);
+
+        intake = hardwareMap.get(DcMotor.class, "intake");
+        intake.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        intake.setPower(0);
+
+        launcher = hardwareMap.get(DcMotor.class, "launcher");
+        launcher.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        launcher.setPower(0);
+
+        flicker = hardwareMap.get(DcMotor.class, "flicker");
+        flicker.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        flicker.setPower(0);
+
+        rotator = hardwareMap.get(DcMotor.class, "rotator");
+        rotator.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rotator.setPower(0);
+
+//        limelight = hardwareMap.get(Limelight3A.class, "limelight");
+
+//        telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
+
+        waitForStart();
+        follower.startTeleopDrive();
+        runtime.reset();
+        while (opModeIsActive()){
+            time = runtime.time();
+
+            double y = -gamepad2.left_stick_y; //forward/backward
+            double x = gamepad2.left_stick_x; //strafe (left/right)
+            double r = gamepad2.right_stick_x; //rotate
+
+            //bot movements
+            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(r), 1);
+            double leftFrontPower = (y + x + r) / denominator;
+            double leftRearPower = (y - x + r) / denominator;
+            double rightFrontPower = (y - x - r) / denominator;
+            double rightRearPower = (y + x - r) / denominator;
+
+            leftFront.setPower(leftFrontPower);
+            leftRear.setPower(leftRearPower);
+            rightFront.setPower(rightFrontPower);
+            rightRear.setPower(rightRearPower);
+
+            boolean xPressed = gamepad1.x && !xLast;
+            xLast = gamepad1.x;
+
+            if (xPressed){
+                newTime = runtime.time();
+                launcher.setPower(1);
+                while (newTime-time<2){
+                    newTime = runtime.time();
+                }
+                intake.setPower(-1);
+                flicker.setPower(1);
+                while (newTime-time<2.5){
+                    newTime = runtime.time();
+                }
+                intake.setPower(0);
+                flicker.setPower(0);
+                while (newTime-time<4){
+                    newTime = runtime.time();
+                }
+                intake.setPower(-1);
+                flicker.setPower(1);
+                while (newTime-time<4.5){
+                    newTime = runtime.time();
+                }
+                intake.setPower(0);
+                flicker.setPower(0);
+                while (newTime-time<6){
+                    newTime = runtime.time();
+                }
+                intake.setPower(-1);
+                flicker.setPower(1);
+
+
+            }
+
+            //intake movements
+            if (gamepad1.left_bumper){
+                intake.setPower(1);
+            }
+            else if (gamepad1.right_bumper){
+                intake.setPower(-1);
+            }
+            else {
+                intake.setPower(0);
+            }
+            //launcher movements
+            launcher.setPower(gamepad1.right_trigger);
+            flicker.setPower(gamepad1.left_trigger);
+
+
+            if (gamepad1.dpad_left){
+                rotator.setPower(-1);
+            }
+            else if (gamepad1.dpad_right){
+                rotator.setPower(1);
+            }
+            else {
+                rotator.setPower(0);
+            }
+
+            telemetry.update();
+        }
+    }
+    //for organization. Sets both intakes' power in the same direction
+
+}
+
+
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
@@ -3,16 +3,44 @@ package org.firstinspires.ftc.teamcode.pedroPathing;
 import com.pedropathing.follower.Follower;
 import com.pedropathing.follower.FollowerConstants;
 import com.pedropathing.ftc.FollowerBuilder;
+import com.pedropathing.ftc.drivetrains.MecanumConstants;
+import com.pedropathing.ftc.localization.constants.PinpointConstants;
 import com.pedropathing.paths.PathConstraints;
+import com.qualcomm.hardware.gobilda.GoBildaPinpointDriver;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
 public class Constants {
     public static FollowerConstants followerConstants = new FollowerConstants();
 
     public static PathConstraints pathConstraints = new PathConstraints(0.99, 100, 1, 1);
+    public static MecanumConstants driveConstants = new MecanumConstants()
+            .maxPower(1)
+            .rightFrontMotorName("rightFront")
+            .rightRearMotorName("rightRear")
+            .leftRearMotorName("leftRear")
+            .leftFrontMotorName("leftFront")
+            .leftFrontMotorDirection(DcMotorSimple.Direction.REVERSE)
+            .leftRearMotorDirection(DcMotorSimple.Direction.REVERSE)
+            .rightFrontMotorDirection(DcMotorSimple.Direction.FORWARD)
+            .rightRearMotorDirection(DcMotorSimple.Direction.FORWARD);
+    public static PinpointConstants localizerConstants = new PinpointConstants()
+            .forwardPodY(-5)
+            .strafePodX(0.5)
+            .distanceUnit(DistanceUnit.INCH)
+            .hardwareMapName("pinpoint")
+            .encoderResolution(GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_4_BAR_POD)
+            .forwardPodY(-4)
+            .strafePodX(4.5)
+            .forwardEncoderDirection(GoBildaPinpointDriver.EncoderDirection.FORWARD)
+            .strafeEncoderDirection(GoBildaPinpointDriver.EncoderDirection.FORWARD);
 
     public static Follower createFollower(HardwareMap hardwareMap) {
         return new FollowerBuilder(followerConstants, hardwareMap)
+                .pinpointLocalizer(localizerConstants)
+                .mecanumDrivetrain(driveConstants)
                 .pathConstraints(pathConstraints)
                 .build();
     }
