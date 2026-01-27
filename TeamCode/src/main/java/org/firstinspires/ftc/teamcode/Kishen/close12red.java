030b8a5b4f18c5988aa61e785e2c768c3a2fd950
kcoffey27 <kieraaurora@outlook.com>
2026-01-27 15:38:35 -0500

    edits

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Kishen/close12red.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Kishen/close12red.java
@@ -113,24 +113,24 @@ public class close12red extends OpMode {
 
     PathState pathState;
     private final Pose startPose = new Pose(117.6, 130, Math.toRadians(36.5));
-    private final Pose shootPose1 = new Pose(85, 88, Math.toRadians(47));
+    private final Pose shootPose1 = new Pose(85, 88, Math.toRadians(48.5));
     private final Pose collect1thingstart=new Pose(85, 84, Math.toRadians(0));
 
 
     private final Pose collect1thing = new Pose(120, 84, Math.toRadians(0));
     private final Pose openGateStart = new Pose(120, 77, Math.toRadians(90));
     private final Pose openGateEnd = new Pose(122, 77, Math.toRadians(90));
-    private final Pose shootPose2 = new Pose( 87, 84, Math.toRadians(47));
+    private final Pose shootPose2 = new Pose( 87, 84, Math.toRadians(48.5));
 
 
     private final Pose collect2Start = new Pose(88, 57.5, Math.toRadians(0));
     private final Pose collect2End = new Pose(127, 57.5, Math.toRadians(0));
-    private final Pose shootBall3 = new Pose(87, 84, Math.toRadians(47));
+    private final Pose shootBall3 = new Pose(87, 84, Math.toRadians(51));
     private final Pose collect3start=new Pose(95, 35, Math.toRadians(0));
 
-
+//
     private final Pose collect3end = new Pose(127, 35, Math.toRadians(0));
-    private final Pose shootBall4 = new Pose(87, 84, Math.toRadians(47));
+    private final Pose shootBall4 = new Pose(87, 84, Math.toRadians(50));
     private final Pose park = new Pose(103, 84, Math.toRadians(46));
 
 
@@ -218,7 +218,7 @@ public class close12red extends OpMode {
             case start:
                 // Try to use limelight for initial adjustment, fallback to hardcoded values
                 launcher.setVelocity(1850); //1725
-                hood.setPosition(0.25); //0.285
+                hood.setPosition(0.28); //0.285
                 follower.setMaxPower(NORMAL_DRIVE_POWER);
                 follower.followPath(shoot1);
                 setPathState(close12red.PathState.actuallyshoot1);
@@ -248,7 +248,7 @@ public class close12red extends OpMode {
                 if (!follower.isBusy()) {
                     follower.setMaxPower(INTAKE_DRIVE_POWER);
                     launcher.setVelocity(1850);
-                    hood.setPosition(0.25);
+                    hood.setPosition(0.29);
                     theWheelOfTheOx.setPower(1);
                     tree.setPower(1);
                     follower.followPath(collect1);
@@ -314,7 +314,7 @@ public class close12red extends OpMode {
                     launcher.setVelocity(1800);
                     follower.followPath(collect2);
                     tree.setPower(1);
-                    hood.setPosition(0.25);
+                    hood.setPosition(0.275);
                     theWheelOfTheOx.setPower(1);
                     //theWheelOfTheOx.setPower(0.005);
                     //hood.setPosition(0.225);
@@ -351,7 +351,7 @@ public class close12red extends OpMode {
                     launcher.setVelocity(1800);
                     follower.followPath(collect3);
                     tree.setPower(1);
-                    hood.setPosition(0.25);
+                    hood.setPosition(0.29);
                     theWheelOfTheOx.setPower(1);
                     //theWheelOfTheOx.setPower(0.005);
                     //hood.setPosition(0.225);
@@ -363,7 +363,7 @@ public class close12red extends OpMode {
                 if (!follower.isBusy() && pathTimer.getElapsedTimeSeconds() > 1.5 && !shoot4Started) {
                     follower.followPath(shoot4);
                     launcher.setVelocity(1800);
-                    hood.setPosition(0.25);
+                    hood.setPosition(0.28);
                     follower.setMaxPower(NORMAL_DRIVE_POWER);
                     tree.setPower(1);
                     shoot4Started = true; // Mark as started to prevent calling again
